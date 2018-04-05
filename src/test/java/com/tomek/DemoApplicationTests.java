dde178c0f47500dd1cc2e51e714c5212f139ae8e
ZiebaTomasz <32581330+ZiebaTomasz@users.noreply.github.com>
2018-04-05 20:28:51 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/tomek/DemoApplicationTests.java
+++ src/test/java/com/tomek/DemoApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//from github
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
